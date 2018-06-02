949e6f6765ad79932c7047aca9713f2587337595
basketballCzm <1850226700@qq.com>
2018-06-02 16:47:13 +0800

    students attendance analysis

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # attendance_analysis
 分析打卡机导出文件的打卡记录，将相应课程的考勤导出成.xls文件
+
+## 使用说明
+1. python的版本是2.7
+2. 运行该程序python要安装pip工具，安装xlrd和xlwt包。安装参考步骤[pip安装xlrd和xlwt](https://www.jianshu.com/p/28d45b71f15f)
+
+##注意事项
+1. config.txt中的样例时间段的选取必须按照给定的格式填写。例如：
+2018-05-15 07:00:00 11:30:00
+2. 每次删除以前生成的.xls文件后再生成新文件。
---
+++ student_analysis.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*- 
+import operator
+import xlwt
+import types
+
+
+def loadDataset(filename, split_ch, trainingSet=[]):
+	dataset = []
+	for line in open(filename,'r').readlines():
+		m = line.split(split_ch)
+		for item in m:
+			if item.replace('.','').replace('-','').replace('\n','').isdigit():
+				if item.count('.')>0:
+					dataset.append(float(item))
+				else:
+					dataset.append(item)
+					#dataset.append(int(item))
+			else:
+				dataset.append(item)
+		trainingSet.append(dataset)
+		dataset = []
+
+def checkDataset(trainingSet, checkSet, result=[]):
+	for x in range(len(checkSet)):
+		for y in range(len(trainingSet)):
+			if((checkSet[x][0]==trainingSet[y][1][0:10]) & (trainingSet[y][1][11:19]>checkSet[x][1]) & (trainingSet[y][1][11:19]<checkSet[x][2])):
+				result.append(trainingSet[y])
+
+def writeDataset(result):
+	wbk = xlwt.Workbook()
+	sheet = wbk.add_sheet("sheet1")
+	for row in range(len(result)):
+		for col in range(len(result[row])):
+			if(type(result[row][col]) is types.StringType):
+				result[row][col] = result[row][col].decode('utf-8')
+			sheet.write(row,col,result[row][col])
+	wbk.save("student.xls")
+
+def main():
+	trainingSet = []
+	checkSet = []
+	result = []
+	loadDataset('data.dat','\t',trainingSet)
+	loadDataset('config.txt',' ',checkSet)
+
+	checkDataset(trainingSet,checkSet,result)
+	result.sort(key=operator.itemgetter(0))
+	for x in range(len(result)):
+		print(result[x])
+	writeDataset(result)
+
+main()
